66029c718fc1e0035f13fad078011e66b4181c12
hashantr99 <123436883+hashantr99@users.noreply.github.com>
2025-12-03 14:52:43 +0530

    hero_3d_model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { words } from '../constants'
 import Button from '../components/Button'
+import HeroExperience from '../components/HeroModels/HeroExperience'
 
 const Hero = () => {
   return (
@@ -42,6 +43,11 @@ const Hero = () => {
                 </div>
             </header>
             {/* RIGHT: 3D MODEL */}
+            <figure>
+                <div className="hero-3d-layout">
+                    <HeroExperience />
+                </div>
+            </figure>
         </div>
     </section>
   )
